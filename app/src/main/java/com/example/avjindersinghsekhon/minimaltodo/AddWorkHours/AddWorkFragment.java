8a148dde742544807770195d903d7b19507030b9
MrezaGh <mrezagh.legend@gmail.com>
2019-07-20 02:38:11 +0430

    add work UI and IO done
    just working with database is remaining

--- app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AddWorkHours/AddWorkFragment.java
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AddWorkHours/AddWorkFragment.java
@@ -2,8 +2,12 @@ package com.example.avjindersinghsekhon.minimaltodo.AddWorkHours;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.avjindersinghsekhon.minimaltodo.AddToDo.AddToDoFragment;
@@ -12,9 +16,17 @@ import com.example.avjindersinghsekhon.minimaltodo.AppDefault.AppDefaultFragment
 import com.example.avjindersinghsekhon.minimaltodo.R;
 
 import java.util.ArrayList;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+import static android.content.Context.INPUT_METHOD_SERVICE;
 
 public class AddWorkFragment extends AppDefaultFragment {
+
     AnalyticsApplication app;
+    FloatingActionButton mAddWorkFloatingActionButton;
+
     private ArrayList<String> days = new ArrayList<>();
     {
         days.add("Saturdays");
@@ -26,6 +38,11 @@ public class AddWorkFragment extends AppDefaultFragment {
         days.add("Fridays");
     }
 
+    private EditText workFromTime;
+    private EditText workToTime;
+    private Spinner workDays;
+
+
 
 
     @Override
@@ -34,6 +51,35 @@ public class AddWorkFragment extends AppDefaultFragment {
         app = (AnalyticsApplication) getActivity().getApplication();
 
         setDaysSpinner(view);
+        
+        workFromTime = (EditText) view.findViewById(R.id.work_from);
+        workToTime = (EditText) view.findViewById(R.id.work_to);
+        workDays = (Spinner) view.findViewById(R.id.work_days_spinner); 
+        mAddWorkFloatingActionButton = (FloatingActionButton) view.findViewById(R.id.addWorkFloatingActionButton);
+        
+
+        mAddWorkFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (workFromTime.length() <= 0) {
+                    workFromTime.setError(getString(R.string.todo_error));
+                } else {
+                    app.send(this, "Action", "Add Work Hours");
+                    makeResult(RESULT_OK);
+                    getActivity().finish();
+                }
+
+                hideKeyboard(workFromTime);
+                hideKeyboard(workToTime);
+            }
+        });
+
+    }
+
+    private void makeResult(int result) {
+        Log.i("workDay ", workDays.getSelectedItem().toString());
+        Log.i("workFrom ", workFromTime.getText().toString());
+        Log.i("workTo ", workToTime.getText().toString());
 
     }
 
@@ -42,6 +88,12 @@ public class AddWorkFragment extends AppDefaultFragment {
         ArrayAdapter aa = new ArrayAdapter<>(getContext(),R.layout.spinner_item,this.days);
         aa.setDropDownViewResource(R.layout.spinner_item);
         spin.setAdapter(aa);
+    }
+
+    public void hideKeyboard(EditText et) {
+
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(et.getWindowToken(), 0);
     }
 
     @Override
